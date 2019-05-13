ca975ff29a9c8248819f14fd75b9e954be94d417
4uhru3 <50596843+4uhru3@users.noreply.github.com>
2019-05-13 23:37:56 +0300

    Delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=sommers',
-    'username' => 'root',
-    'password' => 'Daemon88',
-    'charset' => 'utf8',
-];
